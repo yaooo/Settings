db7652ab981e1254908d2730c5fe7d2539b8c1e4
Yao <yaoshi616@gmail.com>
2019-02-07 09:23:06 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,21 @@
+# update
+dnf update
+
+dnf install gnome-tweak-tool
+
+# dnf install vlc
+dnf install vlc
+
+dnf install unzip 
+
+# Fedy https://www.folkswithhats.org/
+dnf install https://dl.folkswithhats.org/fedora/$(rpm -E %fedora)/RPMS/fedy-release.rpm
+dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install fedy 
+
+# Fedy built from source
+# Clone and install
+git clone https://github.com/fedy/fedy.git && cd fedy
+sudo make install
+# Clean up
+cd .. && rm -r fedy
